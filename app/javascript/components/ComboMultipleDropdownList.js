fcfb0021f96a4d5e35acd3e06ed1ec47bde0369a
Paul Chavard <github@paul.chavard.net>
2020-12-09 18:08:00 +0100

    refactor ComboboxToken

--- app/javascript/components/ComboMultipleDropdownList.js
+++ app/javascript/components/ComboMultipleDropdownList.js
@@ -1,115 +1,148 @@
-import * as React from "react";
+import React, {
+  useMemo,
+  useState,
+  useRef,
+  useContext,
+  createContext,
+  useEffect,
+  useLayoutEffect
+} from 'react';
 import {
   Combobox,
   ComboboxInput,
   ComboboxList,
   ComboboxOption,
-  ComboboxPopover,
-} from "@reach/combobox";
-import { wrapEvent } from "@reach/utils";
-//import { useCityMatch } from "./utils";
-import "@reach/combobox/styles.css";
+  ComboboxPopover
+} from '@reach/combobox';
+import { wrapEvent } from '@reach/utils';
+import '@reach/combobox/styles.css';
+import matchSorter from 'match-sorter';
+import { fire } from '@utils';
 
-const Context = React.createContext();
+const Context = createContext();
 
-function ComboMultipleDropdownList(params) {
-  let [term, setTerm] = React.useState("");
-  let [selections, setSelections] = React.useState([]);
-  // let results = [{ "city": "Un", "state": "France"}, { "city": "Deux", "state": "Alabama"}, {"city":"Trois", "state": "Quatre"} ]
-  let results = params["options"];
+function ComboMultipleDropdownList({ options, hiddenFieldId, selected }) {
+  const [term, setTerm] = useState('');
+  const [selections, setSelections] = useState(selected);
+  const results = useMemo(() => (term ? matchSorter(options, term) : options), [
+    term
+  ]);
+  const hiddenField = useMemo(
+    () => document.querySelector(`input[data-uuid="${hiddenFieldId}"]`),
+    [hiddenFieldId]
+  );
 
   const handleChange = (event) => {
     setTerm(event.target.value);
   };
 
-  const handleSelect = (value) => {
-    setSelections(selections.concat([value]));
-    setTerm("");
+  const saveSelection = (selections) => {
+    setSelections(selections);
+    if (hiddenField) {
+      hiddenField.setAttribute('value', JSON.stringify(selections));
+      fire(hiddenField, 'autosave:trigger');
+    }
+  };
+
+  const onSelect = (value) => {
+    saveSelection([...selections, value]);
+    setTerm('');
+  };
+
+  const onRemove = (value) => {
+    saveSelection(selections.filter((s) => s !== value));
   };
 
   return (
-    <div>
-      <ExampleTokenbox onSelect={handleSelect}>
-        <ExampleTokenLabel
-          onRemove={(item) => {
-            setSelections(selections.filter((s) => s !== item));
-          }}
+    <Combobox
+      openOnFocus={true}
+      onSelect={onSelect}
+      aria-label="choose an option"
+    >
+      <ComboboxTokenLabel
+        onRemove={onRemove}
+        style={{
+          border: '1px solid #888',
+          display: 'flex',
+          flexWrap: 'wrap'
+        }}
+      >
+        {selections.map((selection) => (
+          <ComboboxToken key={selection} value={selection} />
+        ))}
+        <ComboboxTokenInput
+          value={term}
+          onChange={handleChange}
+          autocomplete={false}
           style={{
-            border: "1px solid #888",
-            display: "flex",
-            flexWrap: "wrap",
+            outline: 'none',
+            border: 'none',
+            flexGrow: 1,
+            margin: '0.25rem',
+            font: 'inherit'
           }}
-        >
-          {selections.map((selection) => (
-            <ExampleToken value={selection} />
-          ))}
-          <ExampleTokenInput
-            value={term}
-            onChange={handleChange}
-            autocomplete={false}
-            style={{
-              outline: "none",
-              border: "none",
-              flexGrow: 1,
-              margin: "0.25rem",
-              font: "inherit",
-            }}
-          />
-        </ExampleTokenLabel>
-        {results && (
-          <ComboboxPopover>
-            {results.length === 0 && (
-              <p>
-                No Results{" "}
-                <button
-                  onClick={() => {
-                    setTerm("");
-                  }}
-                >
-                  clear
-                </button>
-              </p>
-            )}
-            <ComboboxList>
-              {results.slice(0, 10).map((result, index) => (
-                <ComboboxOption
-                  key={index}
-                  value={`${result}`}
-                />
-              ))}
-            </ComboboxList>
-          </ComboboxPopover>
-        )}
-      </ExampleTokenbox>
-    </div>
+        />
+      </ComboboxTokenLabel>
+      {results && (
+        <ComboboxPopover>
+          {results.length === 0 && (
+            <p>
+              No Results{' '}
+              <button
+                onClick={() => {
+                  setTerm('');
+                }}
+              >
+                clear
+              </button>
+            </p>
+          )}
+          <ComboboxList>
+            {results.map((value, index) => (
+              <ComboboxOption key={index} value={value} />
+            ))}
+          </ComboboxList>
+        </ComboboxPopover>
+      )}
+    </Combobox>
   );
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
-function ExampleTokenLabel({ onRemove, onKeyDown, ...props }) {
-  const selectionsRef = React.useRef([]);
-  const [selectionNavIndex, setSelectionNavIndex] = React.useState(-1);
+function ComboboxTokenLabel({ onRemove, onKeyDown, ...props }) {
+  const selectionsRef = useRef([]);
+  const [selectionNavIndex, setSelectionNavIndex] = useState(-1);
 
-  React.useLayoutEffect(() => {
+  useLayoutEffect(() => {
     selectionsRef.current = [];
     return () => (selectionsRef.current = []);
   });
 
   const handleKeyDown = (event) => {
-    if (event.key === "ArrowLeft") {
+    if (event.key === 'ArrowLeft') {
       if (selectionNavIndex > 0) {
         setSelectionNavIndex(selectionNavIndex - 1);
       } else if (selectionsRef.current.length > 0) {
         setSelectionNavIndex(selectionsRef.current.length - 1);
       }
+    } else if (event.key === 'ArrowRight') {
+      if (selectionNavIndex === selectionsRef.current.length - 1) {
+        setSelectionNavIndex(-1);
+      } else if (selectionNavIndex < selectionsRef.current.length - 1) {
+        setSelectionNavIndex(selectionNavIndex + 1);
+      } else if (selectionNavIndex < 0) {
+        setSelectionNavIndex(0);
+      }
+    } else {
+      setSelectionNavIndex(-1);
     }
   };
 
   const context = {
     onRemove,
     selectionsRef,
-    selectionNavIndex,
+    selectionNavIndex
   };
 
   return (
@@ -119,57 +152,69 @@ function ExampleTokenLabel({ onRemove, onKeyDown, ...props }) {
   );
 }
 
-function ExampleToken({ value, ...props }) {
-  const { selectionsRef } = React.useContext(Context);
-  // NEXT: need to know my index so that I can be highlighted on ArrowLeft!
-
-  React.useEffect(() => {
+function ComboboxToken({ value, ...props }) {
+  const { selectionsRef, selectionNavIndex } = useContext(Context);
+  useEffect(() => {
     selectionsRef.current.push(value);
   });
+  const selected = selectionsRef.current[selectionNavIndex] === value;
 
   return (
-    <span style={selectionStyle} {...props}>
+    <span
+      style={
+        selected ? { ...selectionStyle, background: 'green' } : selectionStyle
+      }
+      {...props}
+    >
       {value}
     </span>
   );
 }
 
-function ExampleTokenbox({ onSelect, ...props }) {
-  const handleSelect = () => {};
+function ComboboxTokenInput({ onKeyDown, ...props }) {
+  const { onRemove, selectionsRef, selectionNavIndex } = useContext(Context);
+  const ref = useRef();
+  const handleKeyDown = (event) => {
+    const { value } = event.target;
+    if (
+      event.key === 'Backspace' &&
+      value === '' &&
+      selectionsRef.current.length > 0
+    ) {
+      onRemove(
+        selectionsRef.current[
+          selectionNavIndex === -1
+            ? selectionsRef.current.length - 1
+            : selectionNavIndex
+        ]
+      );
+    } else if (event.key === 'Backspace' && value.length === 1) {
+      setTimeout(() => {
+        ref.current.blur();
+        setTimeout(() => {
+          ref.current.focus();
+        }, 50);
+      }, 50);
+    }
+  };
+
   return (
-    <Combobox
-      onSelect={wrapEvent(onSelect, handleSelect)}
-      aria-label="choose an option"
+    <ComboboxInput
+      ref={ref}
+      onKeyDown={wrapEvent(onKeyDown, handleKeyDown)}
       {...props}
     />
   );
 }
 
-function ExampleTokenInput({ onKeyDown, ...props }) {
-  const { onRemove, selectionsRef } = React.useContext(Context);
-  const handleKeyDown = (event) => {
-    const { value } = event.target;
-    if (
-      event.key === "Backspace" &&
-      value === "" &&
-      selectionsRef.current.length > 0
-    ) {
-      onRemove(selectionsRef.current[selectionsRef.current.length - 1]);
-    }
-  };
-  return (
-    <ComboboxInput onKeyDown={wrapEvent(onKeyDown, handleKeyDown)} {...props} />
-  );
-}
-
 const selectionStyle = {
-  fontSize: "11px",
-  background: "red",
-  border: "solid 1px #aaa",
-  margin: "0.25rem",
-  borderRadius: "1000px",
-  padding: "0.2rem 0.5rem",
-  userSelect: "none",
+  fontSize: '11px',
+  background: 'red',
+  border: 'solid 1px #aaa',
+  margin: '0.25rem',
+  borderRadius: '1000px',
+  padding: '0.2rem 0.5rem',
+  userSelect: 'none'
 };
 
 export default ComboMultipleDropdownList;
